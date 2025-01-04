8335664bacf8d5e748412dd642988ecaa27c1b79
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-05 01:01:13 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -777,6 +777,24 @@ async def stream_logger(message, streamtype):
                 )
             except:
                 pass
+                
+      @app.on_message(filters.command(["logger"]) & OWNER_ID,)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip().lower()
+    if state == "enable":
+        await add_on(2)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(2)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)     
+    except Exception:
+            return     
     
 
 # Change stream & Close Stream
