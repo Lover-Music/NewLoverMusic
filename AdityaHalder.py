ba063d505f51a75355783c37d1d6119d90349e84
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-05 00:47:27 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -775,19 +775,6 @@ async def stream_logger(message, streamtype):
                     parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
                 )
-
-async def logger(client, message, _):
-    usage = _["log_1"]
-    if len(message.command) != 2:
-        return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
-        await add_on(2)
-        await message.reply_text(_["log_2"])
-    elif state == "disable":
-        await add_off(2)
-        await message.reply_text(_["log_3"])
-    else:
         await message.reply_text(usage)
             except:
                 pass
