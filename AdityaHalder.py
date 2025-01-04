f41053e4e547ae9f158ebc22119057af9b973e3b
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-04 23:11:26 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -1,4 +1,4 @@
-0import aiohttp, aiofiles, asyncio, base64, logging
+import aiohttp, aiofiles, asyncio, base64, logging
 import os, platform, random, re, socket
 import sys, time, textwrap
 
@@ -80,7 +80,7 @@ if os.path.exists("Config.env"):
 
 API_ID = int(getenv("API_ID", "22926746"))
 API_HASH = getenv("API_HASH", "ffd91926f59e55fb08b3e1a4f5b99b1d")
-BOT_TOKEN = getenv("BOT_TOKEN", "5624068661:AAFOakklE2ZXSnprLElv1qp0-5bWanBk8dI")
+BOT_TOKEN = getenv("BOT_TOKEN", "6223660988:AAHhkyXjGQaPqNbeNK0KPT6e4VpeO7QaYBg")
 STRING_SESSION = getenv("STRING_SESSION", "BQFd1ZoAweEnwkrlrxuVNq_6FMP3-fWsR5HHsZpgTOlD2NxRvuMkLY2c1dGZfcgNvkCK_y-3_zWYyAGsoBJdG9jUWdy9uW87LXX2jGs5vTGFl1fzBeIqAo8j8HLv8JE0piUnVTc16Wv6HR_fvgyG9oAYxXTSD7DDp9h-GF0nk_GoC-XRSV2hIkjrkISG-9Utz56MWCwwBQ_79wo7mKDjd1_1FN96lXuFxH8_FK2jJkvc6eZ-RFKjnX859jbunMj2fZ68h6zfZrb91FPZWOtsNoucbjzDmpfZZEXLVMYuqY2ol0fau6ROsn4zX8VVXXaBJefgQrmaLz7Xk9PErs4CX8IHAMy-rQAAAAHS5JCGAA")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "7758957839"))
